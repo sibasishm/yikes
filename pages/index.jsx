77259cf45590dd560c8668e0c2564bd3b6c54dd2
Sibasish Mohanty <smsibasish@gmail.com>
2020-08-29 13:53:37 +0530

    assets added and basic markup :done:

---
+++ pages/index.jsx
@@ -0,0 +1,104 @@
+import Head from 'next/head';
+
+const Home = () => {
+	return (
+		<>
+			<Head>
+				<title>Yikes inc.</title>
+				<link rel='stylesheet' href='https://rsms.me/inter/inter.css' />
+			</Head>
+			<header className='hero'>
+				<div className='container'>
+					<h1 className='main-title'>Amazing shoes at an amazing price</h1>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam,
+						ipsam natus excepturi soluta dolore blanditiis ab voluptatibus,
+						ipsa, distinctio quis quasi maiores. Nisi at corrupti, suscipit
+						ipsum delectus maiores quos.
+					</p>
+					<a href='#' className='btn'></a>
+				</div>
+			</header>
+			<main>
+				<section className='featured'>
+					<div className='container'>
+						<h2 className='section-title'>Featured products</h2>
+						<div className='row'>
+							<a href='#' className='featured__item'>
+								<img
+									src='/images/shoe-4.png'
+									alt='A beautiful shoe'
+									className='featured__img'
+								/>
+								<p className='featured__details'></p>
+							</a>
+							<a href='#' className='featured__item'>
+								<img
+									src='/images/shoe-5.png'
+									alt='A beautiful shoe'
+									className='featured__img'
+								/>
+								<p className='featured__details'></p>
+							</a>
+							<a href='#' className='featured__item'>
+								<img
+									src='/images/shoe-6.png'
+									alt='A beautiful shoe'
+									className='featured__img'
+								/>
+								<p className='featured__details'></p>
+							</a>
+						</div>
+					</div>
+				</section>
+				<section className='products'>
+					<div className='conatiner'>
+						<h2 className='section-title'>Our products</h2>
+						<article className='product'>
+							<img
+								src='/images/shoe-1.png'
+								alt='A beautiful shoe'
+								className='product__img'
+							/>
+							<h3 className='product__title'>A really nice shoe</h3>
+							<p className='product_description'>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit.
+								Provident accusamus labore placeat.
+							</p>
+							<a href='#' className='btn'></a>
+						</article>
+						<article className='product'>
+							<img
+								src='/images/shoe-2.png'
+								alt='A beautiful shoe'
+								className='product__img'
+							/>
+							<h3 className='product__title'>The perfect shoe</h3>
+							<p className='product_description'>
+								Totam consectetur a atque natus dolorem distinctio eveniet ad
+								eos voluptates consequuntur nesciunt enim excepturi obcaecati
+								repudiandae.
+							</p>
+							<a href='#' className='btn'></a>
+						</article>
+						<article className='product'>
+							<img
+								src='/images/shoe-3.png'
+								alt='A beautiful shoe'
+								className='product__img'
+							/>
+							<h3 className='product__title'>A shoe to fall in love with</h3>
+							<p className='product_description'></p>
+							<a href='#' className='btn'>
+								Consectetur laborum ratione accusamus ipsam accusantium dolorum
+								officiis, illum exercitationem, nisi labore eveniet!
+							</a>
+						</article>
+					</div>
+				</section>
+			</main>
+		</>
+	);
+};
+
+export default Home;
